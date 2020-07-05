1a4ecd8deaef09028041f6efc8c8ce2a42830028
kelvin53 <geshkoskei53@gmail.com>
2020-07-05 22:52:12 +0300

    add user class to  passlocker.py module

---
+++ passlocker.py
@@ -0,0 +1,18 @@
+class User:
+    """
+    Class that generates new instances of users.
+    """
+
+    user_list = [] # Empty user list
+
+    def __init__(self, username, password):
+        """
+        method that defines the user properties.
+        """
+        self.username = username
+        self.password = password
+
+        
+
+
+
