74f950d286f5ada0dbe8b231b1e6853cec425098
kelvin53 <geshkoskei53@gmail.com>
2020-07-06 00:45:11 +0300

    add test to refactor saving/deleting credentials class

--- passlocker_test.py
+++ passlocker_test.py
@@ -34,8 +34,8 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(User.user_list),1)
 
 
+#setup and class creation up here
 class TestCredentials(unittest.TestCase):
-
     '''
     Test class that defines test cases for the Credentials class behaviours.
 
@@ -57,6 +57,36 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.account,"twitter")
         self.assertEqual(self.new_credential.username,"KI Prono")
         self.assertEqual(self.new_credential.password,"kelvinKIPkost")
+
+
+# other test cases here
+    def test_save_credential(self):
+        '''
+        test_save_credential test case to test if the credential object is saved into
+         the credential list
+        '''
+        self.new_credential.save_credential() # saving the new credential
+        self.assertEqual(len(Contact.credentials_list),1)
+
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credentials.credentials_list = []
+
+
+    def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_credentials to check if we can save multiple credential
+            objects to our credentials_list
+            '''
+            self.new_credential.save_credential()
+            test_save_credential = Credentials("Test","ronny","kEgcsd") # new credential
+            test_save_credential.save_credential()
+            self.assertEqual(len(Credentials.credentials_list),2)
+
+
 
 
 if __name__ == '__main__':
