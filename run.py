2143fe60cae55d5193de50a0876c8c8a2ec0ef05
kelvin53 <geshkoskei53@gmail.com>
2020-07-06 18:35:03 +0300

    modify run.py module

--- run.py
+++ run.py
@@ -87,3 +87,87 @@ def main():
         if login_user == login:
             print(f"Hello {username}.Welcome To Password Locker App")  
             print('\n')
+
+    while True:
+        print("Use these short codes:\n CC - Create a new credential \n DC - Display Credentials \n FC - Find a credential \n GP - Generate A randomn password \n D - Delete credential \n EX - Exit the application \n")
+        short_code = input().lower().strip()
+        if short_code == "cc":
+            print("Create New Credential")
+            print("."*20)
+            print("Account name ....")
+            account = input().lower()
+            print("Your Account username")
+            userName = input()
+            while True:
+                print(" TP - To type your own pasword if you already have an account:\n GP - To generate random Password")
+                password_Choice = input().lower().strip()
+                if password_Choice == 'tp':
+                    password = input("Enter Your Own Password\n")
+                    break
+                elif password_Choice == 'gp':
+                    def random_password(string_length):
+                            letters = string.ascii_letters
+                            return "".join(random.choice(letters) for i in range(string_length))
+                            print(f"Your random password for {username} is:", random_password(8))
+                
+                    break
+                else:
+                    print("Invalid password please try again")
+            save_credentials(create_new_credential(account,username,password))
+            print('\n')
+            print(f"Account Credential for: {account} - UserName: {username} - Password:{password} created succesfully")
+            print('\n')
+        elif short_code == "dc":
+            if display_accounts_details():
+                print("Here's your list of accounts: ")
+                 
+                print('*' * 30)
+                print('_'* 30)
+                for account in display_accounts_details():
+                    print(f" Account:{account.account} \n User Name:{username}\n Password:{password}")
+                    print('_'* 30)
+                print('*' * 30)
+            else:
+                print("You don't have any credentials saved yet..........")
+        elif short_code == "fc":
+            print("Enter the Account Name you want to search for")
+            search_name = input().lower()
+            if find_credential(search_name):
+                search_credential = find_credential(search_name)
+                print(f"Account Name : {search_credential.account}")
+                print('-' * 50)
+                print(f"User Name: {search_credential.username} Password :{search_credential.password}")
+                print('-' * 50)
+            else:
+                print("That Credential does not exist")
+                print('\n')
+        elif short_code == "d":
+            print("Enter the account name of the Credentials you want to delete")
+            search_name = input().lower()
+            if find_credential(search_name):
+                search_credential = find_credential(search_name)
+                print("_"*50)
+                search_credential.delete_credentials()
+                print('\n')
+                print(f"Your stored credentials for : {search_credential.account} successfully deleted!!!")
+                print('\n')
+            else:
+                print("That Credential you want to delete does not exist in your store yet")
+
+        elif short_code == 'gp':
+            def random_password(string_length):
+                            letters = string.ascii_letters
+                            return "".join(random.choice(letters) for i in range(string_length))
+                            print(f"Your random password for {username} is:", random_password(8))
+                
+            print(f" {password} Has been generated succesfull. You can proceed to use it to your account")
+        elif short_code == 'ex':
+            print("Thanks for using passwords locker.. See you next time!")
+            break
+        else:
+            print("Wrong entry... Check your entry again and let it match those in the menu")
+    else:
+        print("Please enter a valid input to continue")
+
+if __name__ == '__main__':
+    main()       
