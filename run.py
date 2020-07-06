80b96b28e5047886bacf7b359c56b263c3569da9
kelvin53 <geshkoskei53@gmail.com>
2020-07-06 16:03:21 +0300

    add code to run module

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 from passlocker import User,Credentials
-
+import random #Generate random numbers for various distributions including integer and floats.
+import string #string module allows you to create and customize your own string
 
 def create_user(username,password):
     '''
@@ -22,4 +23,43 @@ def del_user(user):
     Function to delete a user
     '''
     user.delete_user()
+
+def create_new_credential(account,username,password):
+    """
+    Function to create a new credential for a given user
+    """
+    new_credential = Credentials(account,username,password)
+    return new_credential
+def save_credentials(credentials):
+    """
+    Function to save Credentials to the credentials list
+    """
+    credentials. save_details()
+def display_accounts_details():
+    """
+    Function that returns all the saved credential.
+    """
+    return Credentials.display_credentials()
+
+def delete_credential(credentials):
+    """
+    Function to delete a Credentials from credentials list
+
+    """
+    credentials.delete_credentials()
+
+def find_credential(account):
+    """
+    Function that finds a Credentials by an account name and returns the Credentials that belong to that account
+    """
+    return Credentials.find_credential(account)
+
+def generate_Password():
+    '''
+    generates a random password for the user.
+    '''
+    auto_password=Credentials.generatePassword()
+    return auto_password
+
+
 
